6fdf63e508e7cb2a7a512caa76aa4e302913aa5e
aravindan1412 <aravindankalaiyarasu@gmail.com>
2024-09-22 18:08:32 -0700

    Updated the answers to Exercise_3.cpp

--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h> 
 using namespace std; 
   
-// A linked list node (changes) 
+// A linked list node 
 class Node  
 {  
     public: 
@@ -14,67 +14,96 @@ to the head of a list and an int, inserts
 a new node on the front of the list. */
 void push(Node** head_ref, int new_data)  
 {  
-    /* 1. allocate node */ 
+    // 1. allocate node
+    Node* new_node = new Node();  
   
-    /* 2. put in the data */  
+    // 2. put in the data
+    new_node->data = new_data;  
   
-    /* 3. Make next of new node as head */ 
+    // 3. Make next of new node as head
+    new_node->next = (*head_ref);  
   
-    /* 4. move the head to point to the new node */
+    // 4. move the head to point to the new node
+    (*head_ref) = new_node;  
 }  
   
-/* Given a node prev_node, insert a new node after the given  
-prev_node */
+/* Given a node prev_node, insert a new node after the given prev_node */
 void insertAfter(Node* prev_node, int new_data)  
 {  
-    /*1. check if the given prev_node is NULL */ 
+    // 1. check if the given prev_node is NULL
+    if (prev_node == NULL)  
+    {  
+        cout << "The given previous node cannot be NULL";  
+        return;  
+    }  
   
-    /* 2. allocate new node */ 
+    // 2. allocate new node
+    Node* new_node = new Node();  
   
-    /* 3. put in the data */ 
+    // 3. put in the data
+    new_node->data = new_data;  
   
-    /* 4. Make next of new node as next of prev_node */
+    // 4. Make next of new node as next of prev_node
+    new_node->next = prev_node->next;  
   
-    /* 5. move the next of prev_node as new_node */ 
+    // 5. move the next of prev_node as new_node
+    prev_node->next = new_node;  
 }  
   
 /* Given a reference (pointer to pointer) to the head  
 of a list and an int, appends a new node at the end */
 void append(Node** head_ref, int new_data)  
 {  
-    /* 1. allocate node */ 
+    // 1. allocate node
+    Node* new_node = new Node();  
   
-    /* 2. put in the data */ 
+    // 2. put in the data
+    new_node->data = new_data;  
   
-    /* 3. This new node is going to be  
-    the last node, so make next of  
-    it as NULL*/  
+    // 3. This new node is going to be the last node, so make next of it as NULL
+    new_node->next = NULL;  
   
-    /* 4. If the Linked List is empty, 
-    then make the new node as head */
+    // 4. If the Linked List is empty, then make the new node as head
+    if (*head_ref == NULL)  
+    {  
+        *head_ref = new_node;  
+        return;  
+    }  
   
-    /* 5. Else traverse till the last node */
+    // 5. Else traverse till the last node
+    Node* last = *head_ref;  
+    while (last->next != NULL)  
+        last = last->next;  
   
-    /* 6. Change the next of last node */ 
+    // 6. Change the next of last node
+    last->next = new_node;  
 }  
   
-// This function prints contents of 
-// linked list starting from head  
-void printList(Node *node)  
+// This function prints contents of linked list starting from head  
+void printList(Node* node)  
 {  
-    //Your code here
+    while (node != NULL)  
+    {  
+        cout << node->data << " ";  
+        node = node->next;  
+    }  
+    cout << endl;
 }  
   
 /* Driver code*/
 int main()  
 {  
     Node* head = NULL;   
+  
+    // Append nodes
     append(&head, 6);  
     push(&head, 7);  
     push(&head, 1);    
     append(&head, 4);    
     insertAfter(head->next, 8);  
-    cout<<"Created Linked list is: ";  
+  
+    cout << "Created Linked list is: ";  
     printList(head);  
+  
     return 0;  
 }  
